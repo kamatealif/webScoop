f9ed8e043e92ca35216312a391192f542cddd542
kamatealif <kamatealip1@gmail.com>
2025-08-18 10:58:20 +0530

    Refactor image scraping function to support pagination and dynamic target count

--- image_scraping/main.py
+++ image_scraping/main.py
@@ -22,13 +22,12 @@ def download_image(image_url, filename):
     except Exception as e:
         print(f"Error downloading {image_url}: {e}")
 
-def scrape_and_download_images(base_url, image_dir, target_count=10):
+def scrape_and_download_images(base_url, image_dir, target_count=20, count=0):
     url = base_url;
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    books = soup.select('article.product_pod')[:target_count]
-
+    books = soup.select('article.product_pod')
 
     if not os.path.exists(image_dir):
         os.mkdir(image_dir)
@@ -46,12 +45,28 @@ def scrape_and_download_images(base_url, image_dir, target_count=10):
         print(f"Downloading image for book: {title} from {image_url}")
         download_image(image_url, filename=filepath)
         print()
-    print(f"Downloaded {len(books)} images to {image_dir}")
 
+        count += 1
+        print(f"Downloaded {count} books so far...")
+        if count >= target_count:
+            break
+
+    print(f"Downloaded {count} images to {image_dir}")
+    next_link = soup.select_one('li.next > a')
+    next_url = urljoin(base_url, next_link.get('href')) if next_link else None
+
+    if next_url and count < target_count:
+        print(f"Next page URL: {next_url}")
+        scrape_and_download_images(next_url, image_dir, target_count, count)
+    else:
+        print("No more pages to scrape or target count reached.")
+
+    print("Image scraping completed.")
+    current_url = next_url
 
 if __name__ == "__main__":
     BASE_URL = "https://books.toscrape.com/"
     IMAGE_DIR = 'images'
+    TARGET_COUNT = int(input("Enter the number of books to scrape images for: "))
 
-    TARGET_COUNT = 10
     scrape_and_download_images(BASE_URL, IMAGE_DIR, TARGET_COUNT)
